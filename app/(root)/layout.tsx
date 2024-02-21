3c56a431877a0827b5b287a2f20d4b57d68b9bea
Nabid <nabid.explore@gmail.com>
2024-02-21 20:34:15 +0600

    sidebar and mobilenav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import Sidebar from '../../components/shared/Sidebar'
+import MobileNav from '@/components/shared/MobileNav'
 
 const layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className="root">
+    <main className="root">
+      <Sidebar />
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">
           {children}
         </div>
       </div>
-    </div>
+    </main>
   )
 }
 
